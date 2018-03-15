a353db7ca6c9d963fb675900f6f4f494280fc8b7
Mauricio Sarmiento <micho.sarmiento.b@gmail.com>
2018-03-15 13:12:51 -0700

    added no repeat operators and evaluate result for double operators

--- index1.js
+++ index1.js
@@ -1,43 +1,53 @@
 
 // Show
 let show = document.querySelector('#show');
-
-// Operators
-let deleteAll = document.querySelector('#deleteAll');
-let backOne = document.querySelector('#backOne');
-let equal = document.querySelector('#total');
 //Numbers
-var pressed = document.querySelectorAll(".pressed");
-
-
+let pressed = document.querySelectorAll(".pressed");
+// Operators
 let operators = ['+', '-', '*', '/'];
 let operators2 = ['.'];
 let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Results
 let input = [];
-let result;
+let result = 0;
 
+// Initial state
+show.innerHTML = result;
+
+// Logic when pressed any button
 pressed.forEach(press => {
   press.addEventListener('click', ()=>{
     var inner = press.innerHTML;
     if(inner === '=' || inner === 'AC' || inner === 'CE'){
       deleteKey(inner);
     } else {
-      input.push(inner);
-      updateShow();
+      updateShow(inner);
     }
   });
 });
 
-function updateShow(){
-  show.innerHTML = input.join('');
+
+// Logic updating show 
+function updateShow(obj){
+  //check if input is 0 you can do equal or press any of the operators
+  if(input.length < 1 && (operators.includes(obj))){
+    console.log('error its an operator');
+  // checks second to last input and if its an operator and the new input is also an operator it doesnt let you. 
+  } else if (operators.includes(input[input.length-1]) && (operators.indexOf(obj) !== -1) || operators2.includes(input[input.length-1]) && (operators2.indexOf(obj) !== -1)){
+    console.log('double operator');
+  }  else {
+    input.push(obj);
+    show.innerHTML = input.join('');
+  }
 }
 
+// Logic when deleting or equal
 function deleteKey (obj) {
   if(obj === "CE"){
     input.pop();
     show.innerHTML = input.join('');
   } else if (obj === '='){
-    evaluate();
+    evaluateResult(obj);
   } else if (obj === "AC"){
     input = [];
     result = '';
@@ -45,13 +55,27 @@ function deleteKey (obj) {
   }
 }
 
-function evaluate(){
+// Evaluate the result. 
+function evaluateResult(obj){
   result = input.join('');
-  if(eval(result) % 1 === 0){
-    show.innerHTML = eval(result);
-    input = [eval(result)];
+  // checks if there is an operators at the second to last index and if the following is a dot and we press equal then it will delete the +
+  if(operators.includes(input[input.length - 2 ]) && operators2.includes(input[input.length -1])){
+    input.pop();
+    result = input.join('');
+    show.innerHTML = result;
+    console.log('error there is an operators and an operator 1 together');
+  // checks if when pressed equal and there is nothing in the array then don nothing and show 0 at show
+  } else if(input.length < 1){
+    show.innerHTML = 0;
+    console.log('Equal pressed with no numbers in input');
+  // checks if the resutl has decimals 
+  } else if(eval(result) % 1 === 0){
+    show.innerHTML = Number(eval(result));
+    input = [Number(eval(result))];
   } else {
-    show.innerHTML = eval(result).toFixed(2);
-    input = [eval(result).toFixed(2)];
+  // if result has decimals then it shows only two.
+    show.innerHTML = Number(eval(result).toFixed(2));
+    input = [Number(eval(result).toFixed(2))];
   }
 }
+
